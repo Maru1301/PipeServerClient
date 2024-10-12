fe5580ede8f694b09048d8c7afb6a94a6612b001
MaruLin <how7633818@gmail.com>
2024-10-12 10:31:11 +0800

    加入專案檔案。

---
+++ NamedPipeClient.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Pipes;
+
+class NamedPipeClient
+{
+    static void Main()
+    {
+        using (NamedPipeClientStream pipeClient = new("26.102.129.179", "TestPipe", PipeDirection.InOut))
+        {
+            Console.WriteLine("Connecting to server...");
+            pipeClient.Connect();
+            Console.WriteLine("Connected to server.");
+
+            using (StreamReader reader = new StreamReader(pipeClient))
+            {
+                string message = reader.ReadLine();
+                Console.WriteLine("Received: " + message);
+            }
+        }
+    }
+}
